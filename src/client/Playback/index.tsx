0db413774a92f67cb0e5f817864b0ad5ccc55e44
Wilson Lin <code@wilsonl.in>
2021-06-28 00:54:15 +1000

    Implement custom progress bar

--- src/client/Playback/index.tsx
+++ src/client/Playback/index.tsx
@@ -1,10 +1,14 @@
+import assertExists from "extlib/js/assertExists";
 import classNames from "extlib/js/classNames";
 import mapDefined from "extlib/js/mapDefined";
 import { Duration } from "luxon";
-import React, { MutableRefObject, useEffect, useState } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { ListedAudio, ListedVideo } from "../../api/listFiles";
 import { useElemDimensions } from "../_common/ui";
 import "./index.css";
+
+const getRatio = (pageX: number, rect: DOMRect) =>
+  (pageX - rect.left) / rect.width;
 
 export default ({
   canShowCard,
@@ -38,6 +42,60 @@ export default ({
   useEffect(() => {
     onDetailsButtonVisibilityChange(showDetailsButton);
   }, [showDetailsButton]);
+
+  // We don't use a simple input[type=range] for several reasons:
+  // - Difficulty in cross-browser/-platform styling.
+  // - Bad onTimeUpdate => value => onChange loop.
+  // - Not actually that smooth on all devices.
+  const [scrubbingOffset, setScrubbingOffset] = useState<number | undefined>(
+    undefined
+  );
+  const [scrubbingRect, setScrubbingRect] = useState<DOMRect | undefined>(
+    undefined
+  );
+  const scrubbingDebounce = useRef<any>();
+  // Use a fake fill percentage when scrubbing to have perceived smooth scrubbing while still using a debounce.
+  const [scrubbingOverride, setScrubbingOverride] = useState<
+    number | undefined
+  >(undefined);
+  // Avoid using pointer* events, there are still lots of browser and platform
+  // inconsistencies and bugs.
+  useEffect(() => {
+    const listener = () => {
+      setScrubbingOffset(undefined);
+      setScrubbingOverride(undefined);
+    };
+    const EVENTS = ["mouseup", "touchend", "touchcancel"];
+    for (const e of EVENTS) {
+      document.addEventListener(e, listener, true);
+    }
+    return () => {
+      for (const e of EVENTS) {
+        document.removeEventListener(e, listener, true);
+      }
+    };
+  }, []);
+  useEffect(() => {
+    const listener = (e: MouseEvent | TouchEvent) => {
+      const pageX = "touches" in e ? e.touches[0].pageX : e.pageX;
+      if (scrubbingOffset != undefined && element) {
+        const ratio = getRatio(pageX, assertExists(scrubbingRect));
+        setScrubbingOverride(ratio * 100);
+        clearTimeout(scrubbingDebounce.current);
+        scrubbingDebounce.current = setTimeout(() => {
+          if (element) {
+            element.currentTime = element.duration * ratio;
+          }
+        }, 100);
+      }
+    };
+    const EVENTS = ["mousemove", "touchmove"] as const;
+    for (const e of EVENTS) document.addEventListener(e, listener, true);
+    return () => {
+      clearTimeout(scrubbingDebounce.current);
+      for (const e of EVENTS) document.removeEventListener(e, listener, true);
+    };
+  }, [scrubbingOffset, element]);
 
   return (
     <div
@@ -150,19 +208,30 @@ export default ({
           <div className="playback-progress-top">
             <div>&nbsp;</div>
           </div>
-          <input
+          <div
             className="playback-slider"
-            type="range"
-            min={0}
-            max={totalTime.toMillis()}
-            step={1}
-            value={currentTime.toMillis()}
-            onChange={(e) => {
+            onPointerDown={(e) => {
+              setScrubbingOffset(e.clientX);
+              const rect = e.currentTarget.getBoundingClientRect();
+              setScrubbingRect(rect);
+              const ratio = getRatio(e.pageX, rect);
               if (element) {
-                element.currentTime = e.currentTarget.valueAsNumber / 1000;
+                element.currentTime = element.duration * ratio;
               }
             }}
-          />
+          >
+            <div className="playback-slider-tube">
+              <div
+                className="playback-slider-fill"
+                style={{
+                  width: `${
+                    scrubbingOverride ??
+                    (currentTime.toMillis() / totalTime.toMillis()) * 100
+                  }%`,
+                }}
+              />
+            </div>
+          </div>
           <div className="playback-progress-bottom">
             <div>-{totalTime.minus(currentTime).toFormat("m:ss")}</div>
             <div>{currentTime.toFormat("m:ss")}</div>
