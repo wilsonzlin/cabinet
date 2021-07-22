2aa5be3cdfd05040e69fe9339d47add87c0ae838
Wilson Lin <code@wilsonl.in>
2021-07-22 20:21:08 +1000

    Fix search

--- src/library/search.ts
+++ src/library/search.ts
@@ -70,6 +70,8 @@ export class FsSearch {
           // Use [...dir, ""].join(sep) instead of dir.join(sep) + sep in case sep needs to be escaped.
           // If dir.length == 0, [...dir, ""].join(sep) will result in "/" which is not what we want.
           params.push([...dir, ""].join(sep).replace(/[*%_]/g, "*$&") + "%");
+        } else {
+          where.push("true");
         }
       } else {
         where.push(`dir = ?`);
